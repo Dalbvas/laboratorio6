49ea2c4fc30528d736fc4be57926ba9c9f05b36b
Dalbvas <52789866+Dalbvas@users.noreply.github.com>
2019-09-17 15:39:55 -0600

    Update script.js

--- Laboratorio 6/js/script.js
+++ Laboratorio 6/js/script.js
@@ -6,12 +6,29 @@ function generar() {
   var menor=100;
   var cantidadNumeros = 15;
   var myArray = []
+
   while (myArray.length < cantidadNumeros) {
-    var numeroAleatorio = Math.ceil(Math.random() * cantidadNumeros);
+    var numeroAleatorio = Math.round(Math.random() * 100);
     myArray[myArray.length] = numeroAleatorio;
   }
   console.log("números aleatorios : " + myArray);
 
+  var canvas = document.getElementById("canvas1");
+  var ctx = canvas.getContext("2d");
+
+  var width = 50;
+  var X = 2;
+
+  for (i = 0; i < myArray.length; i++) {
+    ctx.fillStyle = '#008080';
+    var h = myArray[i];
+    ctx.fillRect(X, canvas.height - h, width, h);
+
+    X += width + 15;
+
+    ctx.fillStyle = '#4da6ff';
+    ctx.fillText(myArray[i], X - 50, canvas.height - h - 10);
+  }
   const bubbleSort = myArray => {
     const l = myArray.length;
     for (let i = 0; i < l; i++) {
@@ -30,18 +47,23 @@ function generar() {
 
     return myArray;
   };
-  const result = bubbleSort(myArray);
 
+  const result = bubbleSort(myArray);
   console.log("números ordenados : " + result);
   console.log("el numero mayor es:" + mayor);
   console.log("el numero menor es:"+ menor);
 
-  var canvas = document.getElementById("canvas");
+  
+  
+  
+
+  var canvas = document.getElementById("canvas2");
   var ctx = canvas.getContext("2d");
 
   var width = 50;
   var X = 2;
 
+  //Recorre el arreglo para poder graficarlo
   for (i = 0; i < myArray.length; i++) {
     ctx.fillStyle = '#008080';
     var h = myArray[i];
@@ -52,10 +74,15 @@ function generar() {
     ctx.fillStyle = '#4da6ff';
     ctx.fillText(myArray[i], X - 50, canvas.height - h - 10);
   }
+
+
 }
 
 function limpiar() {
-  var canvas = document.getElementById('canvas');
+  var canvas = document.getElementById('canvas1');
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var canvas = document.getElementById('canvas2');
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
